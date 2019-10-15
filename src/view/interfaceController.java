3eb0166fb143b10479a666671596d0443a776f70
Jiang-JD <37043133+Jiang-JD@users.noreply.github.com>
2019-10-15 14:17:11 +0800

    更新计算器fxml和Controller
    添加控件id和点击事件，以handle<控件>命名

--- src/view/interfaceController.java
+++ src/view/interfaceController.java
@@ -1,8 +1,203 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent;
+
 /**
- * 计算器界面 Controller，控制GUI控件
+ * 计算器界面控制器
  */
 public class interfaceController {
+
+    @FXML
+    private Button two_btn;
+
+    @FXML
+    private Button one_btn;
+
+    @FXML
+    private Text equation_text;
+
+    @FXML
+    private Button percent_btn;
+
+    @FXML
+    private Button dot_btn;
+
+    @FXML
+    private Button equal_btn;
+
+    @FXML
+    private Button backspace_btn;
+
+    @FXML
+    private Button minus_btn;
+
+    @FXML
+    private Button fraction_btn;
+
+    @FXML
+    private Button addition_btn;
+
+    @FXML
+    private Button multiply_btn;
+
+    @FXML
+    private Button division_btn;
+
+    @FXML
+    private Button six_btn;
+
+    @FXML
+    private Button clearerror_btn;
+
+    @FXML
+    private Text answer_text;
+
+    @FXML
+    private Button seven_btn;
+
+    @FXML
+    private Button three_btn;
+
+    @FXML
+    private Button four_btn;
+
+    @FXML
+    private Button square_btn;
+
+    @FXML
+    private Button eight_btn;
+
+    @FXML
+    private Button root_btn;
+
+    @FXML
+    private Button clear_btn;
+
+    @FXML
+    private Button nine_btn;
+
+    @FXML
+    private Button five_btn;
+
+    @FXML
+    private Button zero_btn;
+
+    @FXML
+    void handleClearError(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleBackSpace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handlePercent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleFraction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRoot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSquare(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDivision(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSeven(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNine(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMul(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleFour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleFive(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSix(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMinus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleOne(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTwo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleThree(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleZero(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEqual(ActionEvent event) {
+
+    }
 
 }
