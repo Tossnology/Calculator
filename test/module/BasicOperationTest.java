0684938bf2dd6f1f84072c1b3c6de0f0ebf0590f
Jiang-JD <37043133+Jiang-JD@users.noreply.github.com>
2019-10-15 14:25:33 +0800

    添加单元测试

---
+++ test/module/BasicOperationTest.java
@@ -0,0 +1,37 @@
+package module;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * 基本运算测试类
+ */
+public class BasicOperationTest {
+
+    /*
+     * Test Strategy:
+     *
+     *
+     */
+
+    @Test
+    public void getNumberA() {
+    }
+
+    @Test
+    public void getNumberB() {
+    }
+
+    @Test
+    public void setNumberA() {
+    }
+
+    @Test
+    public void setNumberB() {
+    }
+
+    @Test
+    public void getResult() {
+    }
+}
---
+++ test/module/SelfOperationTest.java
@@ -0,0 +1,23 @@
+package module;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * 一元运算符测试类
+ */
+public class SelfOperationTest {
+
+    @Test
+    public void getNumber() {
+    }
+
+    @Test
+    public void setNumber() {
+    }
+
+    @Test
+    public void getResult() {
+    }
+}
