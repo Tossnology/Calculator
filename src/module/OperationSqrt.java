b7812a0db546dbf2f718360372934ba44bb4de02
yRXX <2793128739@qq.com>
2019-10-16 12:22:56 +0800

    上传部分基本计算的类

--- src/module/OperationSqrt.java
+++ src/module/OperationSqrt.java
@@ -1,8 +1,16 @@
 package module;
 
+import static java.lang.Math.sqrt;
+
 public class OperationSqrt extends SelfOperation{
     @Override
     public double getResult(){
-        return 0;
+        try {
+            result = sqrt(getNumber());
+        } catch (Exception e) {
+            System.out.println("不能为负数");
+            e.printStackTrace();
+        }
+        return  result;
     }
 }
